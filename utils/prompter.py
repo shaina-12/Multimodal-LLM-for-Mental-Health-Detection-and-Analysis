4850abf6c04806e9d053d78c13ae093cf0dd766e
Sonik Sarungale <142231915+sarungalesonik@users.noreply.github.com>
2024-11-19 13:40:48 +0530

    Finetune audio features prompter

--- utils/prompter.py
+++ utils/prompter.py
@@ -15,9 +15,9 @@ class Prompter(object):
         if not template_name:
             # Enforce the default here, so the constructor can be called with '' and will not break.
             template_name = "alpaca"
-        file_name = osp.join("../templates", f"{template_name}.json")
+        file_name = osp.join("/media/nas_mount/shaina_mehta/baseline/ltu_new/ltu/src/ltu_as/templates", f"{template_name}.json")
         if not osp.exists(file_name):
-            file_name = osp.join("./templates", f"{template_name}.json")
+            file_name = osp.join("/media/nas_mount/shaina_mehta/baseline/ltu_new/ltu/src/ltu_as/templates", f"{template_name}.json")
         if not osp.exists(file_name):
             raise ValueError(f"Can't read {file_name}")
         with open(file_name) as fp:
